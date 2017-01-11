7abfe42973ab9712886411daa446749f8669b4f5
dermot <dermot.kilroy@invoko.co.uk>
2017-01-11 07:02:13 +0000

    basic dotnet core structure

---
+++ dotnet_core_and_docker/src/app/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+using Library;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"the answer is {new Thing().Get(19, 23)}");
+        }
+    }
+}
---
+++ dotnet_core_and_docker/test/test-library/Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Library;
+using Xunit;
+
+namespace Tests
+{
+    public class LibraryTests
+    {
+        [Fact]
+        public void TestThing() 
+        {
+            Console.WriteLine("dotnet core on a mac (in docker) ftw!");
+            Assert.Equal(43, new Thing().Get(19, 23));
+        }
+    }
+}
