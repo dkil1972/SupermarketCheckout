52c2b00ad5c4d9ad60c574b50166250ed21fc0e0
dermot <dermot.kilroy@invoko.co.uk>
2017-01-14 20:26:55 +0000

    added the first test and some infrastructure

--- dotnet_core_and_docker/src/library/Thing.cs
+++ dotnet_core_and_docker/src/library/Thing.cs
@@ -1,11 +1,59 @@
 ﻿using System;
 using static Newtonsoft.Json.JsonConvert;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Library
 {
-    public class Thing
+    public class Checkout
     {
-        public int Get(int left, int right) =>
-            DeserializeObject<int>($"{left + right}");
+        private IRepository _pricingRepository; 
+
+        public Checkout(IRepository repository)
+        {
+            _pricingRepository = repository;
+        }
+        public decimal CalculatePriceFor(string items)
+        {
+            var products = _pricingRepository.GetAll(items);
+
+            return items.ToCharArray().Sum(
+                    item => products.First(product => product.Sku == item)
+                        .CalculatePriceWithDiscount(item));
+        }
+    }
+
+    public interface IRepository
+    {
+        IEnumerable<Product> GetAll(string itemsInBasket);
+    }
+
+    public class Product
+    {
+        public char Sku {get; private set; }
+        public decimal BasePrice {get; private set; }
+        public PercentageOffer Offer {get; private set; }
+
+        public Product(char sku, decimal basePrice, PercentageOffer offer)
+        {
+            Sku = sku;
+            BasePrice = basePrice;
+            Offer = offer;
+        }
+
+        public decimal CalculatePriceWithDiscount(char sku)
+        {
+            return -1;
+        }
+    }
+
+    public class PercentageOffer
+    {
+        private decimal _multiplier;
+
+        public PercentageOffer(decimal multiplier)
+        {
+            _multiplier = multiplier;
+        }
     }
 }
--- dotnet_core_and_docker/test/test-library/Tests.cs
+++ dotnet_core_and_docker/test/test-library/Tests.cs
@@ -1,16 +1,40 @@
 ﻿using System;
 using Library;
 using Xunit;
+using System.Collections.Generic;
 
 namespace Tests
 {
     public class LibraryTests
     {
         [Fact]
-        public void TestThing() 
+        public void should_calculate_discounted_price_of_a_single_item_when_offer_can_be_applied() 
         {
-            Console.WriteLine("dotnet core on a mac (in docker) ftw!");
-            Assert.Equal(43, new Thing().Get(19, 23));
+            var data = new List<Product>
+            {
+                new Product('A', 50m, new PercentageOffer(0.9m))
+            };
+
+            var pricingRepository = new InMemoryRepository(data);
+            var checkout = new Checkout(pricingRepository);
+            var discountedPrice = checkout.CalculatePriceFor("A");
+            Assert.Equal(45m, discountedPrice);
         }
+    }
+
+    public class InMemoryRepository : IRepository
+    {
+        private IEnumerable<Product> _dataToReturn;
+
+        public InMemoryRepository(IEnumerable<Product> dataToReturn)
+        {
+            _dataToReturn = dataToReturn;
+        }
+
+        public IEnumerable<Product> GetAll(string itemsInBasket)
+        {
+            return _dataToReturn;
+        }
+
     }
 }
